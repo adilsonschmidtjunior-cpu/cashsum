c997c8e30bf5ada92dcd1150186000d2998da01a
adilsonschmidtjunior-cpu <adilsonschmidtjunior@gmail.com>
2026-07-13 02:38:56 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,36 +1,14 @@
-import Script from "next/script";
-
 export const metadata = {
   title: "CashSum",
   description: "Count your notes and coins in seconds.",
   icons: {
-    icon: "/logo.png",
+    icon: "/favicon-source.png",
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <head>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1120468337160701"
-          crossOrigin="anonymous"
-          strategy="beforeInteractive"
-        />
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-0BK72152P5"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-0BK72152P5');
-          `}
-        </Script>
-      </head>
       <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
