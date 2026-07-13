e0725c1a49f74a15afe83cc2fac8db40876d5e57
adilsonschmidtjunior-cpu <adilsonschmidtjunior@gmail.com>
2026-07-13 01:44:50 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export const metadata = {
   title: "CashSum",
   description: "Count your notes and coins in seconds.",
@@ -9,6 +11,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0BK72152P5"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0BK72152P5');
+          `}
+        </Script>
+      </head>
       <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
