d71d46b9c2716e9e002f8c26e64fc574f77d72d8
adilsonschmidtjunior-cpu <adilsonschmidtjunior@gmail.com>
2026-08-04 11:39:50 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export const metadata = {
   title: "CashSum",
   description: "Count your notes and coins in seconds.",
@@ -9,6 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1120468337160701"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
